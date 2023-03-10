6b74711fec4099ab6dfe4fa6ab33d517943e4789
Kavishwar Wagholikar <waghsk@gmail.com>
2023-03-10 07:29:09 -0500

    Update and rename prerequisites.md to prerequisites.sh

--- prerequisites.md
+++
@@ -1,6 +0,0 @@
-### pre-requisites: docker-compose on ubuntu 22.04
-
-  1. curl -fsSL https://get.docker.com -o get-docker.sh
-  2. sudo sh get-docker.sh
-  3. sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  4. sudo chmod +x /usr/local/bin/docker-compose
---
+++ prerequisites.sh
@@ -0,0 +1,9 @@
+### pre-requisites: installing docker-compose on ubuntu 22.04
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker $USER
+newgrp docker
+### Log out and log back in so that your group membership is re-evaluated.(reboot linux machine)
